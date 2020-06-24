a6daa1d50a573a79253117991639739add5c947e
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-06-23 22:58:05 -0400

    code quiz

--- assets/script.js
+++ assets/script.js
@@ -47,47 +47,7 @@ function startTimer() {
 playButton.addEventListener("click", startTimer);
 
 
-// var mainEl = document.getElementById("main");
-// var readEl = document.getElementById("read");
-// var timerEl = document.getElementById("countdown");
-// var bodyEl = document.createElement("div");
 
-// var i = 0;
-
-// // var millisecondsPerWord = prompt("How many milliseconds between words would you like?");
-
-// function prepareRead() {
-//   var timeLeft = 5;
-
-//   var timeInterval = setInterval(function() {
-//     timerEl.textContent = timeLeft + " ";
-//     timeLeft--;
-
-//     if (timeLeft < 0) {
-//       timerEl.textContent = "";
-//       speedRead();
-//       clearInterval(timeInterval);
-//     }
-
-//   }, 1000);
-// }
-
-
-// function speedRead() {
-//   mainEl.append(bodyEl);
-
-//   var poemInterval = setInterval(function() {
-//     if (words[i] === undefined) {
-//       clearInterval(poemInterval);
-//     } else {
-//       mainEl.textContent = words[i];
-//       i++;
-//     }
-
-//   }, millisecondsPerWord);
-// }
-
-//prepareRead();
 
 // create a list of questions and answers
 var questionList = [
@@ -215,7 +175,7 @@ function buttonHandler(event) {
           questionIndex++;
           initializeQuestion();
           score.textContent= "";
-      }, 5000);
+      }, 1000);
   }
   else{
       score.textContent = "You got it wrong";
@@ -223,7 +183,7 @@ function buttonHandler(event) {
           questionIndex++;
           initializeQuestion();
           score.textContent= "";
-      }, 5000);
+      }, 1000);
   }
 }
 
